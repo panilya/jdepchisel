a87c57860dcca70d11b7f03db75d01c73cfe3c49
panilya <panilyau@gmail.com>
2022-07-10 18:15:48 +0300

    Clean unused imports in Main class

--- src/main/java/com/panilya/jdepchisel/JDependencyChisel.java
+++ src/main/java/com/panilya/jdepchisel/JDependencyChisel.java
@@ -1,17 +1,14 @@
 package com.panilya.jdepchisel;
 
-import com.panilya.jdepchisel.constantpool.ConstantPoolReader;
 import com.panilya.jdepchisel.graph.DependencyGraph;
 import com.panilya.jdepchisel.graph.DependencyGraphCreator;
-
-import java.util.Map;
 
 public class JDependencyChisel {
 
     public static void main(String[] args) {
         try {
             DependencyGraphCreator graphCreator = new DependencyGraphCreator();
-            DependencyGraph dependencyGraph = graphCreator.generate("com.panilya.jdepchisel.graph.DependencyGraph"); // Enter root class e.g. entry point for deps searching
+            DependencyGraph dependencyGraph = graphCreator.generate("com.panilya.jdepchisel.JDependencyChisel"); // Enter root class e.g. entry point for deps searching
             for (var classNodeEntry : dependencyGraph.processedClasses.entrySet()) {
                 var className = classNodeEntry.getKey();
                 var classValue = classNodeEntry.getValue().dependsOn;
