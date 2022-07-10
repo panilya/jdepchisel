56f9d341a48c5fcf6986fffdeb3a1c1fafaa84a0
panilya <panilyau@gmail.com>
2022-07-10 18:48:14 +0300

    Added link to Graphviz Online and make output correct for use

--- src/main/java/com/panilya/jdepchisel/JDependencyChisel.java
+++ src/main/java/com/panilya/jdepchisel/JDependencyChisel.java
@@ -9,6 +9,7 @@ public class JDependencyChisel {
         try {
             DependencyGraphCreator graphCreator = new DependencyGraphCreator();
             DependencyGraph dependencyGraph = graphCreator.generate("com.panilya.jdepchisel.JDependencyChisel"); // Enter root class e.g. entry point for deps searching
+            System.out.println("digraph classDependencies {");
             for (var classNodeEntry : dependencyGraph.getProcessedClasses().entrySet()) {
                 var className = classNodeEntry.getKey();
                 var classValue = classNodeEntry.getValue().dependsOn;
@@ -17,6 +18,8 @@ public class JDependencyChisel {
                     System.out.println("\"" + className + "\" -> \"" + depends.classFile.getClassName() + "\";");
                 }
             }
+            System.out.println("}");
+            System.out.println("https://sketchviz.com/new" + "Graphviz Online Visualizer in order to see graph");
         } catch (Exception e) {
             e.printStackTrace();
         }
